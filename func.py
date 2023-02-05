20fe744744a3aabf7087fc66eccc455c7e726f34
po3tt <kadet3443@mail.ru>
2023-02-05 21:42:45 +0300

    d

--- func.py
+++ func.py
@@ -1,9 +1,15 @@
 import datetime
+from datetime import timedelta
+slovoform = {
+    "завтра": datetime.datetime.now() + timedelta(days=1)
+}
+
+
 
 def learn_notify(notify_msg):
     what = notify_msg[0]
-    when = notify_msg[1]
-    dt=datetime.datetime.now()
-    dt.strptime(when, "%d.%m.%Y %H:%M")
-    print(what)
-    print(when)
+    when = str(notify_msg[1])
+    print(datetime.datetime.now() + timedelta(days=1).strftime("%Y.%d.%m %H:%M"))
+    print(datetime.datetime.strptime(when, "%Y.%d.%m %H:%M"))
+    
+

bde71243c12fd7d2387b81c347ed6f95a71fbaba
po3tt <kadet3443@mail.ru>
2023-02-15 22:55:33 +0300

    nwe

--- func.py
+++ func.py
@@ -1,31 +1,44 @@
-import datetime
-from datetime import timedelta
+from datetime import timedelta, datetime, date, time
 
 
 slovoform = {
-    "завтра": "="+str((datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%d.%m.%Y')),
-    "послезавтра" : "="+str((datetime.datetime.now() + datetime.timedelta(days=2)).strftime('%d.%m.%Y'))
+    "завтра": str((datetime.now() + timedelta(days=1)).strftime('%d.%m.%Y')),
+    "послезавтра" : str((datetime.now() + timedelta(days=2)).strftime('%d.%m.%Y')),
+    "через" : "ввввввввввввввввв"
 }
 
-def check_in_date(when): #функция проверки есть ли эта дата в словаре
-    
-    return False
-
-
+slovoform2 = {
+    "день": str((datetime.now() + timedelta(days=1)).strftime('%d.%m.%Y')),
+    "неделя" : str((datetime.now() + timedelta(days=2)).strftime('%d.%m.%Y')),
+    "месяц" : "ввввввввввввввввв"
+}
 
 def learn_notify(notify_msg):
     what = notify_msg[0]
     when = str(notify_msg[1])
-    date = check_in_date(when)
+    if "через" in when:
+        data = when.split()
+        print(data)
+        return 
+
     for i,j in slovoform.items():
             if when in i: 
                 return j
-    if "=" in date:
-        return (date[1:])
-    elif len(str(date)) <=10 and date!="f":
-        return datetime.datetime.strptime(when, "%d.%m.%Y")
-    elif date=="f":
-        return (datetime.datetime.strptime(when, "%d.%m.%Y %H:%M"))
-    
+    if len(str(when)) <=10:
+        return datetime.strptime(when, "%d.%m.%Y").date().strftime('%d.%m.%Y')
+    else:
+        return (datetime.strptime(when, "%d.%m.%Y %H:%M").strftime('%d.%m.%Y %H:%M'))
 
 
+text = ("""sdfsdfsd-20.03.2022 12:55
+11ййййййй-завтра
+4343453453-послезавтра
+выаываыва-через 1 день
+sdfsdfsd-20.02.2022""")
+
+add_noty = [i.split("-") for i in text.split("\n")]
+db=[]
+for i in add_noty:
+    print(str(learn_notify(i)))
+    #db.append([datetime.now().strftime("%d.%m.%Y %H:%M"),i[0],,"",""])
+#print(db)
